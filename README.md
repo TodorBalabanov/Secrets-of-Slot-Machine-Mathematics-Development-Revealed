9d3930e3de93f51e21e9cda7b539f2fb86e8d712
Todor Balabanov <todor.balabanov@gmail.com>
2025-09-14 16:24:25 +0300

    RNG instance has been added.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Secrets-of-Slot-Machine-Mathematics-Development-Revealed
-Secrets of Slot Machine Mathematics Development Revealed
+# Secrets of Slot Machine Mathematics Development Revealed
+
+The repository collects supporting materials for the book "Secrets of Slot Machine Mathematics Development Revealed."
+
+## Building the Plugin
+
+clear && gradlew clean build deploy
+
+## Formatting the Source Code
+astyle --style=java --indent=tab --recursive *.java
--- simulator/src/main/java/eu/veldsoft/bths/Simulator.java
+++ simulator/src/main/java/eu/veldsoft/bths/Simulator.java
@@ -1,24 +1,27 @@
 package eu.veldsoft.bths;
 
+import java.security.SecureRandom;
+
 import com.sun.star.script.provider.XScriptContext;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.frame.XModel;
 import com.sun.star.sheet.XSpreadsheetDocument;
 import com.sun.star.container.XIndexAccess;
 import com.sun.star.sheet.XSpreadsheet;
-import com.sun.star.table.XCell;
 
 public class Simulator {
-    public static void simulate(XScriptContext ctx) {
-        try {
-            XModel model = ctx.getDocument();
-            XSpreadsheetDocument document = UnoRuntime.queryInterface(XSpreadsheetDocument.class, model);
-            XIndexAccess sheets = UnoRuntime.queryInterface(XIndexAccess.class, document.getSheets());
-            XSpreadsheet first = UnoRuntime.queryInterface(XSpreadsheet.class, sheets.getByIndex(0));
+	private static final SecureRandom PRNG = new SecureRandom();
 
-            first.getCellByPosition(0, 0).setFormula("Simulation!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public static void simulate(XScriptContext ctx) {
+		try {
+			XModel model = ctx.getDocument();
+			XSpreadsheetDocument document = UnoRuntime.queryInterface(XSpreadsheetDocument.class, model);
+			XIndexAccess sheets = UnoRuntime.queryInterface(XIndexAccess.class, document.getSheets());
+			XSpreadsheet first = UnoRuntime.queryInterface(XSpreadsheet.class, sheets.getByIndex(0));
+
+			first.getCellByPosition(0, 0).setFormula("Simulation!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
